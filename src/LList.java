4045dbc4391d34c6db395b8c1479ead17e6b99c3
Michael Chen <chen.michael.1116@gmail.com>
2021-01-13 14:24:57 -0500

    idk

--- src/LList.java
+++ src/LList.java
@@ -46,33 +46,68 @@ public class LList<E>
     }
 
     // Add value to list at position 'index'; do nothing if index is out of bounds
-    public void insert(int index, E value){
-        if (index >= size) {
-            return;
+    public void insert(int index, E value) {
+        if (index < size) {
+            Link<E> newLink = new Link<E>(value);
+            if (index == 0) { // starting from head doesn't work if we want to insert something before head
+                newLink.setNext(head);
+                head = newLink;
+                return;
+            }
+            Link<E> ptr = head; // point to the link which newLink will be inserted after
+            for (int i = 0; i < index - 1; i ++){
+                ptr = ptr.getNext();
+            }
+            newLink.setNext(ptr.getNext());
+            ptr.setNext(newLink);
         }
     }
 
     // Remove and return the value at position 'index' from the list; return null if 'index' is out of bounds
     public E remove(int index){
-
+        if (index == 0){ // head should be updated if removing first element
+            E ret = head.getData();
+            head = head.getNext();
+            return ret;
+        }
+        else if (index > 0 && index < size - 1){
+            Link<E> ptr = head; // point to the link before the link to be removed
+            for (int i = 0; i < index - 1; i ++){
+                ptr = ptr.getNext();
+            }
+            ptr.setNext(ptr.getNext().getNext());
+            return ptr.getData();
+        }
+        else if (index == size - 1){ // tail should be updated if removing last element
+            Link<E> ptr = head; // point to the link before tail
+            for (int i = 0; i < size - 4; i ++){
+                System.out.println("\t" + ptr.getData() + " " + ptr.getNext().getData());
+                ptr = ptr.getNext();
+            }
+            ptr.setNext(null);
+            tail = ptr;
+            return ptr.getData();
+        }
+        return null;
     }
 
     // Return the value at position 'index'; return null if 'index' is out of bounds
     public E get(int index){
-        if (index >= size){
+        if (index >= size) {
             return null;
         }
-        else if (index < size / 2) { // start at head if index is in lower half
-
-        }
-        else { // start at tail if index is in upper half
-
+        else {
+            Link<E> ret = head;
+            for (int i = 0; i < index; i ++){
+                ret = ret.getNext();
+            }
+            return ret.getData();
         }
     }
 
     // Modify the data at 'index' to replace it with 'value', return old value; return null if 'index' is out of bounds
     public E set(int index, E value){
-
+        return null;
     }
 
     // Return number of items in the list
--- src/Tester.java
+++ src/Tester.java
@@ -1,11 +1,19 @@
 public class Tester {
     public static void main(String[] args){
+        /*
         LList<String> test = new LList<String>();
-        test.prepend(" Garbage");
-        test.append(" Collector");
-        test.prepend("Haha");
-        test.append(" go");
-        test.append(" brr");
+        for (char c = 'a'; c <= 'z'; c++){
+            test.append(String.valueOf(c));
+        }
+
         System.out.println(test.toString());
+        test.remove(0);
+        System.out.println(test);
+        test.remove(1);
+        System.out.println(test);
+        test.remove(test.size() - 1);
+        System.out.println(test);
+        */
+
     }
 }
